4751c06bc30d93b5a7797e502437906d27136a1f
JOSE GUADALUPE RODRIGUEZ SASTRE <106033431+N4zgvl@users.noreply.github.com>
2025-11-21 23:49:35 -0600

    ViewModels
    ViewModels

---
+++ PlantTwin/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PlantTwin.Models;
+
+namespace PlantTwin.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<Machine> Machines { get; set; } = new();
+        public List<string> SystemLog { get; set; } = new();
+    }
+}
